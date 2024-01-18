c294cae82b24bcab0706499c04adb25d94fb9838
kasto <kastokniv@live.com>
2024-01-18 12:53:12 +0900

    add build for sanity

---
+++ backend_sanity/dist/static/index-c3743809-28231d13.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-7415496d-52b97ec4.js";import"./sanity-620db5f9.js";import"./index-3d6dad2f.js";export{m as DocumentPaneProvider,a as default};
